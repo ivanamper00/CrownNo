53046e49a730ee88af17268d962c3f1855b7569a
aix-dean <aix-dean@gihub.com>
2022-03-04 16:25:46 +0800

    update bugs

--- common/src/main/java/com/dakuinternational/common/ui/utils/BindingUtils.kt
+++ common/src/main/java/com/dakuinternational/common/ui/utils/BindingUtils.kt
@@ -21,39 +21,43 @@ fun backgroundResource(view: View, @DrawableRes uri: Int){
 
 @BindingAdapter("android:scrUri", "android:imageLoadedListener")
 fun loader(view: ImageView, uri: String, onImageLoaded: OnImageLoaded){
-    Glide.with(view.context)
-        .load(uri)
-        .listener(object: RequestListener<Drawable>{
-            override fun onLoadFailed(
-                e: GlideException?,
-                model: Any?,
-                target: Target<Drawable>?,
-                isFirstResource: Boolean
-            ): Boolean {
-                onImageLoaded.onLoad()
-                return false
-            }
+    try{
+        Glide.with(view.context)
+            .load(uri)
+            .listener(object: RequestListener<Drawable>{
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    onImageLoaded.onLoad()
+                    return false
+                }
 
-            override fun onResourceReady(
-                resource: Drawable?,
-                model: Any?,
-                target: Target<Drawable>?,
-                dataSource: DataSource?,
-                isFirstResource: Boolean
-            ): Boolean {
-                onImageLoaded.onLoad()
-                return false
-            }
-        })
-        .apply( RequestOptions().format(DecodeFormat.PREFER_RGB_565) )
-        .into(view)
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    onImageLoaded.onLoad()
+                    return false
+                }
+            })
+            .apply( RequestOptions().format(DecodeFormat.PREFER_RGB_565) )
+            .into(view)
+    }catch (e: Exception){}
 }
 
 @BindingAdapter("android:uriSrc")
 fun uriSrc(view: ImageView, uri: String){
-    Glide.with(view.context)
-        .load(uri)
-        .apply( RequestOptions().format(DecodeFormat.PREFER_RGB_565) )
-        .into(view)
+    try {
+        Glide.with(view.context)
+            .load(uri)
+            .apply(RequestOptions().format(DecodeFormat.PREFER_RGB_565))
+            .into(view)
+    }catch(e: Exception){}
 }
 
